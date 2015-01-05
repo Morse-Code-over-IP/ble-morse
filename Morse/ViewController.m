0200250ac536c41d3a7133b07112bb0cf4435ee1
Gerolf Ziegenhain <g@ziegenhain.com>
2015-01-05 16:38:44 +0100

    Initial Commit

---
+++ Morse/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Morse
+//
+//  Created by Dr. Gerolf Ziegenhain on 05.01.15.
+//  Copyright (c) 2015 Dr. Gerolf Ziegenhain. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
